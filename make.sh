ed2ced17ee47042f51b72feecb056a0ee390209c
Peter Lazar <peterlazar1993@gmail.com>
2022-01-02 16:42:29 +0530

    fix: fix make script URL

--- make.sh
+++ make.sh
@@ -1,5 +1,5 @@
 
-REPO=https://github.com/Native-Health/zoom-sdk-pods/releases/download/zoom-releases
+REPO=https://github.com/Native-Health/zoom-sdk-pods/releases/download/5.9.1.2180
 FILE=zoom-sdk-ios-5.9.1.2180.zip
 
 wget "$REPO/$FILE"
